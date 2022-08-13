6c853a3a2d91cf7200b30b495c212147d3047d35
architmalhotra2002 <105011253+architmalhotra2002@users.noreply.github.com>
2022-08-13 21:48:47 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main() {
+
+    int a , b ,c;
+    printf("Input a and b\n");
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    printf("\nsum of %d and %d is %d ",a,b,c);
+
+    return 0;
+}
